73ed11e71f03f9e6bb9237289cbefd756dec8061
Gavin M. Bell <gavin@llnl.gov>
2009-12-17 14:26:39 -0800

    Added test class for sanity checking database connectivity and queries.

---
+++ src/java/test/esg/common/db/SimpleDAO.java
@@ -0,0 +1,127 @@
+/***************************************************************************
+*                                                                          *
+*  Organization: Lawrence Livermore National Lab (LLNL)                    *
+*   Directorate: Computation                                               *
+*    Department: Computing Applications and Research                       *
+*      Division: S&T Global Security                                       *
+*        Matrix: Atmospheric, Earth and Energy Division                    *
+*       Program: PCMDI                                                     *
+*       Project: Earth Systems Grid (ESG) Data Node Software Stack         *
+*  First Author: Gavin M. Bell (gavin@llnl.gov)                            *
+*                                                                          *
+****************************************************************************
+*                                                                          *
+*   Copyright (c) 2009, Lawrence Livermore National Security, LLC.         *
+*   Produced at the Lawrence Livermore National Laboratory                 *
+*   Written by: Gavin M. Bell (gavin@llnl.gov)                             *
+*   LLNL-CODE-420962                                                       *
+*                                                                          *
+*   All rights reserved. This file is part of the:                         *
+*   Earth System Grid (ESG) Data Node Software Stack, Version 1.0          *
+*                                                                          *
+*   For details, see http://esg-repo.llnl.gov/esg-node/                    *
+*   Please also read this link                                             *
+*    http://esg-repo.llnl.gov/LICENSE                                      *
+*                                                                          *
+*   * Redistribution and use in source and binary forms, with or           *
+*   without modification, are permitted provided that the following        *
+*   conditions are met:                                                    *
+*                                                                          *
+*   * Redistributions of source code must retain the above copyright       *
+*   notice, this list of conditions and the disclaimer below.              *
+*                                                                          *
+*   * Redistributions in binary form must reproduce the above copyright    *
+*   notice, this list of conditions and the disclaimer (as noted below)    *
+*   in the documentation and/or other materials provided with the          *
+*   distribution.                                                          *
+*                                                                          *
+*   Neither the name of the LLNS/LLNL nor the names of its contributors    *
+*   may be used to endorse or promote products derived from this           *
+*   software without specific prior written permission.                    *
+*                                                                          *
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    *
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      *
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS      *
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE    *
+*   LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR     *
+*   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,           *
+*   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       *
+*   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF       *
+*   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND    *
+*   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,     *
+*   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT     *
+*   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF     *
+*   SUCH DAMAGE.                                                           *
+*                                                                          *
+***************************************************************************/
+
+/**
+   Description:
+
+   Just simply to test full cycle database connectivity.  If this
+   works then we know the path to querying, fetching, processing and
+   presenting data, works.
+
+**/
+package esg.common.db;
+
+import java.util.Properties;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.impl.*;
+
+import esg.common.db.DatabaseResource;
+import esg.node.core.*;
+
+public class SimpleDAO {
+    private static final Log log = LogFactory.getLog(SimpleDAO.class);    
+
+    private static String testQuery = "select name from security.status";
+    private Properties props = null;
+
+    public SimpleDAO() {
+	log.info("Instantiating SimpleDAO...");
+	ESGDataNodeManager datanodeManager = new ESGDataNodeManager();  //do not initialize (call init())
+    }
+
+    public void doTest() {
+	DataSource dataSource = DatabaseResource.getInstance().getDataSource();
+	QueryRunner queryRunner = new QueryRunner(dataSource);
+	String result = "<oops>";
+
+	try{
+	    result = queryRunner.query(testQuery,new ResultSetHandler<String>() {
+		    public String handle(ResultSet rs) throws SQLException {
+			StringBuilder result_ = new StringBuilder();
+			log.trace("Inspecting the result set...");
+			int count = 0;
+			while(rs.next()) {
+			    result_.append("#"+(count++)+":").append(rs.getString("name")).append("\n");
+			}
+			
+			return result_.toString();
+		    }
+		});
+	}catch(SQLException ex) {
+	    log.error(ex);
+	}catch(Throwable t) {
+	    log.error(t);
+	}
+
+	System.out.println("Result:\n[\n"+result+"]");
+	DatabaseResource.getInstance().shutdownResource();
+    }
+    
+    public static void main(String[] args) {
+	(new SimpleDAO()).doTest();   
+    }
+    
+}
