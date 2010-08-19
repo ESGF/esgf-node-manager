99a0db9ec861bc6f8fda8644a01abc0fdf882c80
Gavin M. Bell <gavin@llnl.gov>
2010-08-19 15:05:45 -0700

    Making id the primary key on access_logging table

--- etc/db/node-schema.sql
+++ etc/db/node-schema.sql
@@ -8,7 +8,7 @@ SET client_min_messages = warning;
 SET escape_string_warning = off;
 
 CREATE TABLE  access_logging (
-    id int NOT NULL,
+    id int PRIMARY KEY,
     user_id character varying NOT NULL,
     email character varying,
     url character varying NOT NULL,
