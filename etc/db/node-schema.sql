571bb49ab0ac174152baa5d7ccb2a50b74b402e8
drach1 <drach1@llnl.gov>
2010-08-19 14:57:16 -0700

    - Make id a primary key

--- etc/db/node-schema.sql
+++ etc/db/node-schema.sql
@@ -8,7 +8,7 @@ SET client_min_messages = warning;
 SET escape_string_warning = off;
 
 CREATE TABLE  access_logging (
-    id int NOT NULL,
+    id int PRIMARY KEY,
     userid character varying NOT NULL,
     email character varying NOT NULL,
     url character varying NOT NULL,
