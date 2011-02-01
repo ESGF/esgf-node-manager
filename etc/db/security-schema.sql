bf787a9bd1956009e1c0e96ad0997215da24b8eb
Gavin M. Bell <gavin@llnl.gov>
2011-02-01 09:09:02 -0800

    place holders for registration xml work... and a bit of reoganization

---
+++ etc/db/security-schema.sql
@@ -0,0 +1,42 @@
+--
+-- PostgreSQL database for esg node
+--
+
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+CREATE TABLE  access_logging (
+    id int PRIMARY KEY,
+    user_id character varying NOT NULL,
+    email character varying,
+    url character varying NOT NULL,
+    file_id character varying,
+    remote_addr character varying NOT NULL,
+    user_agent character varying,
+    service_type character varying,
+    batch_update_time double precision,    
+    date_fetched double precision NOT NULL,
+    success boolean DEFAULT false,
+    duration double precision DEFAULT 0
+);
+
+CREATE INDEX idx_access_logging_url ON access_logging USING btree (url);
+CREATE SEQUENCE seq_access_logging;
+ALTER  SEQUENCE seq_access_logging OWNED BY access_logging.id;
+
+CREATE TABLE notification_run_log (
+    id character varying NOT NULL,
+    notify_time double precision NOT NULL
+);
+
+CREATE TABLE monitor_run_log (
+    id character varying NOT NULL,
+    last_run_time double precision NOT NULL
+);
+
+CREATE TABLE metrics_run_log (
+    id character varying NOT NULL,
+    last_run_time double precision NOT NULL
+);
