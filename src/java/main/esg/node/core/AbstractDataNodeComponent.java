6c7f0a7523eaa73aad1478d98f84766932f35396
Gavin M. Bell <gavin@llnl.gov>
2011-05-02 12:23:41 -0700

    (import for Collections missed)

--- src/java/main/esg/node/core/AbstractDataNodeComponent.java
+++ src/java/main/esg/node/core/AbstractDataNodeComponent.java
@@ -65,6 +65,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.impl.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
