4f0a5bf93f51927054c11c351a94e72195e369c0
Gavin M. Bell <gavin.max.bell@sixthcolumn.org>
2011-06-18 00:16:55 -0700

    very first bits of playing around with making a REPL

--- src/java/main/esg/common/shell/ESGFShell.java
+++ src/java/main/esg/common/shell/ESGFShell.java
@@ -1,4 +1,3 @@
-<!--
 /***************************************************************************
 *                                                                          *
 *  Organization: Lawrence Livermore National Lab (LLNL)                    *
@@ -7,7 +6,7 @@
 *      Division: S&T Global Security                                       *
 *        Matrix: Atmospheric, Earth and Energy Division                    *
 *       Program: PCMDI                                                     *
-*       Project: Earth Systems Grid (ESG) Data Node Software Stack         *
+*       Project: Earth Systems Grid Federation (ESGF) Data Node Software   *
 *  First Author: Gavin M. Bell (gavin@llnl.gov)                            *
 *                                                                          *
 ****************************************************************************
@@ -18,11 +17,11 @@
 *   LLNL-CODE-420962                                                       *
 *                                                                          *
 *   All rights reserved. This file is part of the:                         *
-*   Earth System Grid (ESG) Data Node Software Stack, Version 1.0          *
+*   Earth System Grid Federation (ESGF) Data Node Software Stack           *
 *                                                                          *
-*   For details, see http://esg-repo.llnl.gov/esg-node/                    *
+*   For details, see http://esgf.org/esg-node/                             *
 *   Please also read this link                                             *
-*    http://esg-repo.llnl.gov/LICENSE                                      *
+*    http://esgf.org/LICENSE                                               *
 *                                                                          *
 *   * Redistribution and use in source and binary forms, with or           *
 *   without modification, are permitted provided that the following        *
@@ -55,59 +54,100 @@
 *   SUCH DAMAGE.                                                           *
 *                                                                          *
 ***************************************************************************/
+package esg.common.shell;
 
-Description:
-Ivy "settings" file for defining libraries needed for this project.
+/**
+   Description:
+   Top level class for ESGF Shell implementation...
+**/
 
-Note: Check Maven for propery dependency parameter values @
-      http://mvnrepository.com/
--->
+import jline.*;
 
-<ivy-module version="2.0" 
-	    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            xsi:noNamespaceSchemaLocation=
-                      "http://ant.apache.org/ivy/schemas/ivy.xsd">
-  <info organisation="org.esgf" module="esgf-node-manager">
-    <description homepage="http://esgf.org/esgf-node-manager-site/" />
-  </info>
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
 
-  <configurations>
-    <conf name="base" description="required for both compile and runtime" />
-    <conf name="runtime" extends="base" />
-    <conf name="compile" extends="base" />
-    <conf name="test" extends="base" visibility="private" />
-  </configurations>
+public class ESGFShell {
+    public static void usage() {
+        System.out.println("Usage: java " + ESGFShell.class.getName()
+                + " [none/simple/files/dictionary [trigger mask]]");
+        System.out.println("  none - no completors");
+        System.out.println("  simple - a simple completor that comples "
+                + "\"foo\", \"bar\", and \"baz\"");
+        System.out
+                .println("  files - a completor that comples " + "file names");
+        System.out.println("  dictionary - a completor that comples "
+                + "english dictionary words");
+        System.out.println("  classes - a completor that comples "
+                + "java class names");
+        System.out
+                .println("  trigger - a special word which causes it to assume "
+                        + "the next line is a password");
+        System.out.println("  mask - is the character to print in place of "
+                + "the actual password character");
+        System.out.println("\n  E.g - java Example simple su '*'\n"
+                + "will use the simple compleator with 'su' triggering\n"
+                + "the use of '*' as a password mask.");
+    }
 
-  <publications>
-    <artifact name="${module}-common" type="jar" conf="*"/>
-    <artifact name="${module}-filters" type="jar" conf="*"/>
-    <artifact name="${module}-accesslog-client" type="jar" conf="*"/>
-  </publications>
+    public static void main(String[] args) throws IOException {
+        Character mask = null;
+        String trigger = null;
 
-  <dependencies>
-    <dependency org="net.sourceforge.cobertura" name="cobertura" rev="1.9.4"/>
-<!--<dependency org="commons-io" name="commons-io" rev="2.0.1" conf="base->default"/> -->
-    <dependency org="commons-cli" name="commons-cli" rev="1.2"/>
-<!--<dependency org="commons-dbcp" name="commons-dbcp" rev="1.2.2"/> -->
-    <dependency org="commons-dbcp" name="commons-dbcp" rev="1.4"/>
+        ConsoleReader reader = new ConsoleReader();
+        reader.setBellEnabled(false);
+        reader.setDebug(new PrintWriter(new FileWriter("writer.debug", true)));
 
-    <dependency org="commons-dbutils" name="commons-dbutils" rev="1.3"/>
-    <dependency org="commons-httpclient" name="commons-httpclient" rev="3.1"/>
-    <dependency org="commons-lang" name="commons-lang" rev="2.5"/>
-    <dependency org="commons-logging" name="commons-logging" rev="1.1.1"/>
-    <dependency org="commons-pool" name="commons-pool" rev="1.5.4"/>
-<!--<dependency org="com.caucho" name="hessian" rev="3.1.5"/> -->
-    <dependency org="com.caucho" name="hessian" rev="3.0.20"/>
-    <dependency org="com.sun.xml.bind" name="jaxb-xjc" rev="2.2"/>
-    <dependency org="jline" name="jline" rev="0.9.9"/>
-    <dependency org="junit" name="junit" rev="4.8.1"/>
-    <dependency org="log4j" name="log4j" rev="1.2.14">
-      <exclude org="com.sun.jdmk"/>
-      <exclude org="com.sun.jmx"/>
-      <exclude org="javax.jms"/>
-    </dependency>
-    <dependency org="javax.mail" name="mail" rev="1.4.3"/>
-    <dependency org="postgresql" name="postgresql" rev="8.3-603.jdbc3"/>
+        if ((args == null) || (args.length == 0)) {
+            usage();
 
-  </dependencies>
-</ivy-module>
+            return;
+        }
+
+        List completors = new LinkedList();
+
+        if (args.length > 0) {
+            if (args[0].equals("none")) {
+            } else if (args[0].equals("files")) {
+                completors.add(new FileNameCompletor());
+            } else if (args[0].equals("classes")) {
+                completors.add(new ClassNameCompletor());
+            } else if (args[0].equals("dictionary")) {
+                completors.add(new SimpleCompletor(new GZIPInputStream(
+                        ESGFShell.class.getResourceAsStream("english.gz"))));
+            } else if (args[0].equals("simple")) {
+                completors.add(new SimpleCompletor(new String[] { "foo", "bar",
+                        "baz" }));
+            } else {
+                usage();
+
+                return;
+            }
+        }
+
+        if (args.length == 3) {
+            mask = new Character(args[2].charAt(0));
+            trigger = args[1];
+        }
+
+        reader.addCompletor(new ArgumentCompletor(completors));
+
+        String line;
+        PrintWriter out = new PrintWriter(System.out);
+
+        while ((line = reader.readLine("esgfsh> ")) != null) {
+            out.println("======>\"" + line + "\"");
+            out.flush();
+
+            // If we input the special word then we will mask
+            // the next line.
+            if ((trigger != null) && (line.compareTo(trigger) == 0)) {
+                line = reader.readLine("password> ", mask);
+            }
+            if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
+    }
+}
+
