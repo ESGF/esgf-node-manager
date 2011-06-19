d2f5012614874be0737c1de71f21749d379e9a05
Gavin M. Bell <gavin.max.bell@sixthcolumn.org>
2011-06-19 16:26:14 -0700

    test and clearn shell commands

---
+++ src/java/main/esg/common/shell/cmds/ESGFtest.java
@@ -0,0 +1,111 @@
+/***************************************************************************
+*                                                                          *
+*  Organization: Lawrence Livermore National Lab (LLNL)                    *
+*   Directorate: Computation                                               *
+*    Department: Computing Applications and Research                       *
+*      Division: S&T Global Security                                       *
+*        Matrix: Atmospheric, Earth and Energy Division                    *
+*       Program: PCMDI                                                     *
+*       Project: Earth Systems Grid Federation (ESGF) Data Node Software   *
+*  First Author: Gavin M. Bell (gavin@llnl.gov)                            *
+*                                                                          *
+****************************************************************************
+*                                                                          *
+*   Copyright (c) 2009, Lawrence Livermore National Security, LLC.         *
+*   Produced at the Lawrence Livermore National Laboratory                 *
+*   Written by: Gavin M. Bell (gavin@llnl.gov)                             *
+*   LLNL-CODE-420962                                                       *
+*                                                                          *
+*   All rights reserved. This file is part of the:                         *
+*   Earth System Grid Federation (ESGF) Data Node Software Stack           *
+*                                                                          *
+*   For details, see http://esgf.org/esg-node/                             *
+*   Please also read this link                                             *
+*    http://esgf.org/LICENSE                                               *
+*                                                                          *
+*   * Redistribution and use in source and binary forms, with or           *
+*   without modification, are permitted provided that the following        *
+*   conditions are met:                                                    *
+*                                                                          *
+*   * Redistributions of source code must retain the above copyright       *
+*   notice, this list of conditions and the disclaimer below.              *
+*                                                                          *
+*   * Redistributions in binary form must reproduce the above copyright    *
+*   notice, this list of conditions and the disclaimer (as noted below)    *
+*   in the documentation and/or other materials provided with the          *
+*   distribution.                                                          *
+*                                                                          *
+*   Neither the name of the LLNS/LLNL nor the names of its contributors    *
+*   may be used to endorse or promote products derived from this           *
+*   software without specific prior written permission.                    *
+*                                                                          *
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    *
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      *
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS      *
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE    *
+*   LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR     *
+*   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,           *
+*   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       *
+*   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF       *
+*   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND    *
+*   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,     *
+*   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT     *
+*   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF     *
+*   SUCH DAMAGE.                                                           *
+*                                                                          *
+***************************************************************************/
+package esg.common.shell.cmds;
+
+/**
+   Description:
+   ESGF's "test" command..."
+**/
+
+import esg.common.shell.*;
+
+import org.apache.commons.cli.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.impl.*;
+
+public class ESGFtest extends ESGFCommand {
+
+private static Log log = LogFactory.getLog(ESGFtest.class);
+
+    public ESGFtest() {
+        super();
+        //TODO: Define options...
+        getOptions().addOption("t", "test", false, "This is a simple test flag");
+        Option logfile   = OptionBuilder.withArgName( "file" )
+            .hasArg()
+            .withDescription(  "use given file for log" )
+            .create( "testfile" );
+        getOptions().addOption(logfile);
+    }
+
+    public String getCommandName() { return "test"; }
+
+    public ESGFEnv doEval(CommandLine line, ESGFEnv env) {
+        log.info("inside the \"test\" command's doEval");
+        //TODO: Query for options and perform execution logic
+
+        String testfile = null;
+        if(line.hasOption( "testfile" )) {
+            // initialise the member variable
+            testfile = line.getOptionValue( "testfile" );
+            env.getWriter().println("testfile option value is: "+testfile);
+        }
+
+        if(line.hasOption("test")) {
+            env.getWriter().println("just Testing... :-)");
+        }
+        
+        int i=0;
+        for(String arg : line.getArgs()) {
+            log.info("arg("+(i++)+"): "+arg);
+        }
+        
+        return env;
+    }
+}
