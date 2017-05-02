1ee463ec55ae73d9a376a528560d79ac77659225
Sasha Ames <amysash2006@gmail.com>
2017-05-02 15:02:30 -0700

    update secure cred client

--- src/python/client/fetch-pub-credentials.py
+++ src/python/client/fetch-pub-credentials.py
@@ -11,7 +11,7 @@ DEFAULT_KEY = '/etc/grid-security/hostkey.pem'
 
 DIGEST = b'sha256'
 
-def main(insecure):
+def main(insecure, server):
 
 	key = crypto.load_privatekey(crypto.FILETYPE_PEM, open(DEFAULT_KEY).read())
 
@@ -24,11 +24,17 @@ def main(insecure):
 	data['nonce'] = m.digest()
 	data['signature'] = crypto.sign(key, data['nonce'], DIGEST)
 
+	data['action'] = 'get_pub_config'
 
+	url = 'https://' + server  + '/esgf-nm/api'
 
 	verify = not insecure
 
-	res = requests.post(url, data=data, verify=verify)
+	try:
+		res = requests.post(url, data=data, verify=verify)
+	except Exception as e:
+		print e
+		exit(-1)
 
 	print res.text
 
@@ -39,6 +45,6 @@ if __name__ == '__main__':
     # parser.add_option('-c', dest='certificate', help='X.509 certificate path')
     # parser.add_option('-k', dest='key', help='RSA private key path')
     parser.add_argument('-i', '--insecure', action="store_true", help='Insecure operation (does not verify server)')
-
+    parser.add_argument('server', type=str, help="Server FQDN")
     parser.parse_args()
-    main(args.i)
+    main(args.i, args.server)
